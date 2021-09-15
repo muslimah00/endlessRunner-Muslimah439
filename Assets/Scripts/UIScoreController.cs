bceee4e3710c5d5de938479152d906e0676eddf2
muslimah00 <muslimah.mbs@gmail.com>
2021-09-15 23:31:56 +0700

    final game

---
+++ Assets/Scripts/UIScoreController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScoreController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("UI")]
+    public Text score;
+    public Text highScore;
+
+    [Header("Score")]
+    public ScoreController scoreController;
+
+    private void Update()
+    {
+        score.text = scoreController.GetCurrentScore().ToString();
+        highScore.text = ScoreData.highScore.ToString();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# endlessRunner-Muslimah439
- 
+"# 2D-EndlessRunner-Muslimah439" 
